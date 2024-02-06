e57327073ad3341bbc89fa560963b2cc8eaa79e9
John Doe <johndoe@example.com>
2024-02-06 16:58:43 +0100

    fix

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-// Définir les grandes frames de l'animation
 const frames = [
     [
         "     .--.______, ,;_;,",
@@ -38,40 +37,33 @@ const frames = [
     "     !`",
     "     `"
 ]
-    // Ajouter d'autres grandes frames ici
 ];
 
-// Fonction pour envoyer chaque grande frame avec un délai
 function sendFrames(res, frames, index) {
     if (index >= frames.length) {
-        res.end(); // Terminer la réponse lorsque toutes les grandes frames sont envoyées
+        res.end(); 
         return;
     }
 
-    // Effacer la console avant d'afficher la grande frame suivante
-    res.write('\x1B[2J\x1B[0f'); // Codes ANSI pour effacer la console
+    res.write('\x1B[2J\x1B[0f'); 
 
-    // Envoyer chaque ligne de la grande frame
     frames[index].forEach(line => res.write(line + '\n'));
 
     setTimeout(() => {
-        sendFrames(res, frames, index + 1); // Envoyer la prochaine grande frame après un délai
-    }, 100); // Délai de 0.5 seconde (500 millisecondes)
+        sendFrames(res, frames, index + 1);
+    }, 100); 
 }
 
-// Créer un serveur
 const server = http.createServer((req, res) => {
-    // Vérifier si la requête provient de curl
     if (req.headers['user-agent'] && req.headers['user-agent'].includes('curl')) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        sendFrames(res, frames, 0); // Commencer à envoyer les grandes frames
+        sendFrames(res, frames, 0);
     } else {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Please use curl to access this resource.\n');
     }
 });
 
-// Démarrer le serveur sur le port 3000
 const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
